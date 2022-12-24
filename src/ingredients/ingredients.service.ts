d4112f927309ad45b89e8bad48b0d0436a384b7f
Chris-am <c.aranguizm@outlook.com>
2022-12-24 00:56:53 -0300

    :sparkle: adding image updating to update method

--- src/ingredients/ingredients.service.ts
+++ src/ingredients/ingredients.service.ts
@@ -7,7 +7,7 @@ import {
   NotFoundException,
 } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
+import { DataSource, Repository } from 'typeorm';
 //* Third Party Imports
 import { validate as isUUID } from 'uuid';
 //* Own Imports
@@ -19,15 +19,27 @@ import { PaginationDTO } from '../common/DTOs/pagination.dto';
 @Injectable()
 export class IngredientsService {
   private readonly logger = new Logger('Ingredient Service ⚙️ ');
+  private readonly queryRunner = this.dataSource.createQueryRunner();
+
   constructor(
     @InjectRepository(Ingredient)
     private readonly ingredientRepository: Repository<Ingredient>,
     @InjectRepository(IngredientImage)
     private readonly ingredientImageRepository: Repository<IngredientImage>,
+    private readonly dataSource: DataSource,
   ) {}
 
   //* CRUD
-  async create(createIngredientDto: CreateIngredientDto) {
+  private async findOne(searchTerm: string): Promise<Ingredient> {
+    let ingredient: Ingredient;
+    const validatedIngredient = await this.termValidation(
+      searchTerm,
+      ingredient,
+    );
+    return validatedIngredient;
+  }
+
+  public async create(createIngredientDto: CreateIngredientDto) {
     try {
       const { images = [], ...ingredientDetails } = createIngredientDto;
       const ingredient = this.ingredientRepository.create({
@@ -44,9 +56,9 @@ export class IngredientsService {
     return;
   }
 
-  async findAll(paginationDto: PaginationDTO) {
+  public async findAll(paginationDto: PaginationDTO) {
     const { limit = 10, offset = 0 } = paginationDto;
-    console.log('🚀 pagination', limit, offset);
+
     const allIngredients = await this.ingredientRepository.find({
       take: limit,
       skip: offset,
@@ -61,43 +73,42 @@ export class IngredientsService {
     return flatIngredient;
   }
 
-  async findOne(searchTerm: string): Promise<Ingredient> {
-    let ingredient: Ingredient;
-    const validatedIngredient = await this.termValidation(
-      searchTerm,
-      ingredient,
-    );
-    return validatedIngredient;
-  }
-
-  async findOneAndPlainImage(searchTerm: string) {
+  public async findOneAndPlainImage(searchTerm: string) {
     const { images, ...ingredient } = await this.findOne(searchTerm);
     const imgUrl = images.map((image) => image.url);
     return { ...ingredient, imgUrl };
   }
 
-  async update(id: string, updateIngredientDto: UpdateIngredientDto) {
+  public async update(id: string, updateIngredientDto: UpdateIngredientDto) {
+    const { images, ...toUptade } = updateIngredientDto;
+    const ingredient = await this.ingredientRepository.preload({
+      id,
+      ...toUptade,
+    });
+    if (!ingredient) {
+      throw new NotFoundException(
+        `ingrediento ${id} no existe o no se encuentra`,
+      );
+    }
+    await this.prepareRunner();
     try {
-      const ingredient = await this.ingredientRepository.preload({
-        id,
-        ...updateIngredientDto,
-        images: [],
-      });
-      if (!ingredient) {
-        throw new NotFoundException(
-          `ingrediento ${id} no existe o no se encuentra`,
+      if (images) {
+        await this.deleteIngredientImageRunner(id);
+        ingredient.images = images.map((image) =>
+          this.ingredientImageRepository.create({ url: image }),
         );
       }
-      const updatedIngredient = await this.ingredientRepository.save(
-        ingredient,
-      );
-      return updatedIngredient;
+      await this.saveIngredientImageRunner(ingredient);
+      await this.commitRunner();
+      return this.findOneAndPlainImage(id);
     } catch (error) {
+      console.debug('in catch');
+      this.rollbackAndReleaseRunner();
       this.errorHandler(error);
     }
   }
 
-  async remove(id: string) {
+  public async remove(id: string) {
     const doesIngredientExists = await this.findOne(id);
     if (doesIngredientExists) {
       await this.ingredientRepository.remove(doesIngredientExists);
@@ -132,14 +143,44 @@ export class IngredientsService {
   }
 
   private errorHandler(error: any) {
-    // console.log('general errors =>', error);
-    this.logger.error(error);
+    //? just for debugging purpose
+    this.logger.debug(error);
     if (error.code === '23505') {
       throw new BadRequestException(error.detail);
     }
-    if (error.code === '22P02' || error.response.statusCode === 404) {
+    if (error.code === '22P02') {
       throw new NotFoundException('Ingrediente no encontrado');
     }
     throw new InternalServerErrorException('Unexpected error. Check Logs');
+  }
+
+  //* QUERY RUNNERS
+  private async prepareRunner() {
+    await this.queryRunner.connect();
+    await this.queryRunner.startTransaction();
+    return this.queryRunner;
+  }
+  private async deleteIngredientImageRunner(id: string) {
+    //! delete * from IngredientImage where id = :id
+    const deleteImg = await this.queryRunner.manager.delete(IngredientImage, {
+      ingredient: { id },
+    });
+    return deleteImg;
+  }
+
+  private async saveIngredientImageRunner(ingredient: Ingredient) {
+    const save = await this.queryRunner.manager.save(ingredient);
+    return save;
+  }
+
+  private async commitRunner() {
+    await this.queryRunner.commitTransaction();
+    return this.queryRunner;
+  }
+
+  private async rollbackAndReleaseRunner() {
+    await this.queryRunner.rollbackTransaction();
+    await this.queryRunner.release();
+    return this.queryRunner;
   }
 }
