a24dba601600b98c842f28ecc07afa8909b3436c
Chris-am <c.aranguizm@outlook.com>
2022-12-29 07:34:04 -0300

    :technologist: declaring port in env and updating env template with host api and port

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
-import { ValidationPipe } from '@nestjs/common';
+import { ValidationPipe, Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  const logger = new Logger('APP');
+  const port = process.env.PORT;
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
   app.useGlobalPipes(
@@ -11,6 +13,7 @@ async function bootstrap() {
       forbidNonWhitelisted: true,
     }),
   );
-  await app.listen(3000);
+  await app.listen(port);
+  logger.log(`🤖 running on port ${port}`);
 }
 bootstrap();
