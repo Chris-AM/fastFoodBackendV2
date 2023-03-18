33a017291fa86fc91b8601e2aa4884248f22fa1a
Chris-am <c.aranguizm@outlook.com>
2023-03-18 02:31:14 -0300

    :sparkles: just valid users can add or get data

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -13,22 +13,27 @@ import { UserService } from './user.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { PaginationDTO } from '../common/DTOs/pagination.dto';
+import { Auth } from 'src/auth/decorators';
+import { ValidRoles } from 'src/auth/interfaces';
 
 @ApiTags('User')
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
+  @Auth(ValidRoles.ADMIN, ValidRoles.MANAGER, ValidRoles.DEV)
   @Post('create-user')
   create(@Body() createUserDto: CreateUserDto) {
     return this.userService.createUserInPortal(createUserDto);
   }
 
+  @Auth(ValidRoles.ADMIN, ValidRoles.MANAGER, ValidRoles.DEV)
   @Get()
   findAll(@Query() paginationDto: PaginationDTO) {
     return this.userService.findAll(paginationDto);
   }
 
+  @Auth(ValidRoles.ADMIN, ValidRoles.MANAGER, ValidRoles.DEV)
   @Get(':searchTerm')
   findOne(@Param('searchTerm') searchTerm: string) {
     return this.userService.findOneAndPlainAvatar(searchTerm);
--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -3,9 +3,10 @@ import { UserService } from './user.service';
 import { UserController } from './user.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User, UserAvatar } from './entities';
+import { PassportModule } from '@nestjs/passport';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User, UserAvatar])],
+  imports: [TypeOrmModule.forFeature([User, UserAvatar]), PassportModule.register({ defaultStrategy: 'jwt' }),] ,
   controllers: [UserController],
   providers: [UserService],
   exports: [UserService],
